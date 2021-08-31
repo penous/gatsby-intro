1c8242ee95be255b528d19bb395309e659a86255
Tom Dirinck <tom.dirinck@gmail.com>
2021-08-31 16:32:12 +0200

    Complete part 2.

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About Page'>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
