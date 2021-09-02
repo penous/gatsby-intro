1f90e3a616f41f04dbb8d0bb69919d54ef2e9a93
Tom Dirinck <tom.dirinck@gmail.com>
2021-09-02 14:18:22 +0200

    Complete part 3.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-intro",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'gatsby-intro',
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    'gatsby-plugin-sass',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt='Some pretty nature' src='../images/nature.jpeg' />
     </Layout>
   );
 };
