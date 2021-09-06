4579fd854ec1662c1f9f94e05f6506f88b7f3d7a
Tom Dirinck <tom.dirinck@gmail.com>
2021-09-06 16:13:19 +0200

    Complete part 5.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/layout';
 
 const Blog = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
       <ul>
-        {data.allFile.nodes.map((node, i) => {
-          return <li key={i}>{node.name}</li>;
+        {data.allMdx.nodes.map((node) => {
+          return (
+            <article key={node.id}>
+              <h1>{node.frontmatter.title}</h1>
+              <p>{node.frontmatter.date}</p>
+              <MDXRenderer>{node.body}</MDXRenderer>
+            </article>
+          );
         })}
       </ul>
     </Layout>
@@ -16,22 +23,17 @@ const Blog = ({ data }) => {
 
 export const query = graphql`
   {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        body
       }
     }
   }
 `;
-
-// export const query = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         description
-//       }
-//     }
-//   }
-// `;
 
 export default Blog;
